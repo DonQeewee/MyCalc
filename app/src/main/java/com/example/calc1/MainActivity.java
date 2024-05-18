ddb936aeee804350646d2cc8a17001daa50f8618
Ayomide <primedaug@outlook.com>
2024-05-18 12:17:30 +0100

    Calculator Assignment

--- app/src/main/java/com/example/calc1/MainActivity.java
+++ app/src/main/java/com/example/calc1/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
     double firstNum;
+    double secondNum;
+
     String operation;
 
     @Override
@@ -35,6 +37,36 @@ public class MainActivity extends AppCompatActivity {
             firstNum = 0;
             binding.screen.setText("0");
         });
+        binding.num0.setOnClickListener(v -> {
+            binding.screen.setText("0");
+        });
+        binding.num1.setOnClickListener(v -> {
+            binding.screen.setText("1");
+        });
+        binding.num2.setOnClickListener(v -> {
+            binding.screen.setText("2");
+        });
+        binding.num3.setOnClickListener(v -> {
+            binding.screen.setText("3");
+        });
+        binding.num4.setOnClickListener(v -> {
+            binding.screen.setText("4");
+        });
+        binding.num5.setOnClickListener(v -> {
+            binding.screen.setText("5");
+        });
+        binding.num6.setOnClickListener(v -> {
+            binding.screen.setText("6");
+        });
+        binding.num7.setOnClickListener(v -> {
+            binding.screen.setText("7");
+        });
+        binding.num8.setOnClickListener(v -> {
+            binding.screen.setText("8");
+        });
+        binding.num9.setOnClickListener(v -> {
+            binding.screen.setText("9");
+        });
 
         ArrayList<Button> nums = new ArrayList<>();
         nums.add(binding.num0);
@@ -48,51 +80,67 @@ public class MainActivity extends AppCompatActivity {
         nums.add(binding.num8);
         nums.add(binding.num9);
 
-        for (Button b: nums){
-            if (!binding.screen.getText().toString().equals("0")){
+        for (Button b : nums) {
+            if (!binding.screen.getText().toString().equals("0")) {
                 binding.screen.setText(binding.screen.getText().toString() + b.getText().toString());
-            }else {
+            } else {
                 binding.screen.setText(b.getText().toString());
             }
         }
+
+        binding.div.setOnClickListener(v -> {
+            binding.screen.setText("/");
+        });
+        binding.times.setOnClickListener(v -> {
+            binding.screen.setText("X");
+        });
+        binding.minus.setOnClickListener(v -> {
+            binding.screen.setText("-");
+        });
+        binding.plus.setOnClickListener(v -> {
+            binding.screen.setText("+");
+        });
+
 
         ArrayList<Button> operators = new ArrayList<>();
         operators.add(binding.div);
         operators.add(binding.times);
         operators.add(binding.minus);
         operators.add(binding.plus);
-        for (Button b: operators){
+        for (Button b : operators) {
             b.setOnClickListener(v -> {
                 firstNum = Double.parseDouble(binding.screen.getText().toString());
                 operation = b.getText().toString();
-                binding.screen.setText("0");
+                binding.screen.setText(b.getText().toString());
+
             });
         }
 
         binding.del.setOnClickListener(v -> {
             String num = binding.screen.getText().toString();
-            if (num.length()>1){
-                binding.screen.setText(num.substring(0,num.length()-1));
-            } else if (num.length()==1 && !num.equals("0")) {
-                binding.screen.setText("'0");
+            if (num.length() > 1) {
+                binding.screen.setText(num.substring(0, num.length() - 1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                binding.screen.setText("0");
             }
         });
 
         binding.point.setOnClickListener(v -> {
-            if (!binding.screen.getText().toString().contains(".")){
+            binding.screen.setText(".");
+            if (!binding.screen.getText().toString().contains(".")) {
                 binding.screen.setText(binding.screen.getText().toString() + ".");
             }
         });
 
         binding.equals.setOnClickListener(v -> {
-            double secondNum = Double.parseDouble(binding.screen.getText().toString());
+            secondNum = Double.parseDouble(binding.screen.getText().toString());
             double result;
-            switch (operation){
+            switch (operation) {
                 case "/":
-                    result = firstNum/secondNum;
+                    result = firstNum / secondNum;
                     break;
                 case "X":
-                    result = firstNum*secondNum;
+                    result = firstNum * secondNum;
                     break;
                 case "+":
                     result = firstNum + secondNum;
